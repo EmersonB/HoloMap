616c36896db1e8c6cd90358f0229803f2c3d752c
Emerson Berlik <eberlik@gmail.com>
2018-06-05 10:36:09 -0400

    finished proj

---
+++ Assets/MinimapImageScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MinimapImageScript : MonoBehaviour
+{
+    public string myURL = "http://ec2-54-197-12-180.compute-1.amazonaws.com:3000/api/alpha/map";
+
+
+    void Start()
+    {
+        StartCoroutine(GrabWWWTexture());
+    }
+
+    IEnumerator GrabWWWTexture()
+    {
+        while (true)
+        {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(myURL, false);
+            www.SetRequestHeader("Accept", "image/*");
+            Debug.LogError("Downloading...");
+            yield return www.SendWebRequest();
+            while (!www.isDone)
+            {
+                Debug.LogError(".");
+                yield return null;
+            }
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+                gameObject.GetComponent<Renderer>().materials[0].mainTexture = myTexture;
+                Debug.Log("Success");
+            }
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
